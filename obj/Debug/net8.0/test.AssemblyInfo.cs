eb84818c991df0ae221a7c7d2e0b70c66ea5645f
centuryhopper <43508492+centuryhopper@users.noreply.github.com>
2024-08-06 21:46:17 -0400

    update

--- obj/Debug/net8.0/test.AssemblyInfo.cs
+++ obj/Debug/net8.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5a9bcfab590c4f4308c3c594c9e1d87d48e65b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec141f3158e83ff47e9051ae05c5bd5206a3edd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
